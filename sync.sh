d96172a1119748141e9d2b88e8809025969cb594
Luca Bataillard <lucabad@bluewin.ch>
2021-12-11 22:38:03 +0100

    add marker subgroup lib

--- sync.sh
+++ sync.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 for value in assets _data _includes _layouts _sass _config.yml index.html thumbnail.png
 do
-    cp -r ../project/website/$value .
+    cp -r ../project/website/"$value" .
 done
 
-git commit -am "$1" 
-git push
+if [ -z "$1" ]
+then
+  echo "Files copied, uncommited"
+else
+  git add .;
+  git commit -m "$1";
+  git push;
+fi
